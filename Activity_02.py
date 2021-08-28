2204ccba714d8bce7c88ee99dc9a8c069aa24943
SREENATH <84218337+Sreenath09@users.noreply.github.com>
2021-08-28 13:33:16 +0530

    Update Activity_02.py

--- Activity_02.py
+++ Activity_02.py
@@ -1,3 +1,5 @@
 #Write your code here:
-========================= RESTART: C:/Users/sreen/Downloads/Python/hello.py ==============================
+>>> 
+=============== RESTART: C:/Users/sreen/Downloads/Python/hello.py ==============
+Hello World!
 >>> 
